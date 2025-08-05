82bdcbe47cab272cdf1eb2c58f352b6a2db5cede
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2025-08-05 15:29:38 -0500

    2nd

--- ShoppingCart/src/shoppingcart/ShoppingCart.java
+++ ShoppingCart/src/shoppingcart/ShoppingCart.java
@@ -7,18 +7,18 @@ import java.util.Iterator;
 import products.Product;
 
 public class ShoppingCart {
-	ArrayList<CartLine> list = new ArrayList<CartLine>();
+	ArrayList<CartLine> cartLines = new ArrayList<CartLine>();
 
 	public void printProducts() {
 		System.out.println("Content of the shoppingcart:");
-		for (CartLine cline : list) {
+		for (CartLine cline : cartLines) {
 			System.out.println(STR."\{cline.getQuantity()} \{cline.getProduct().getProductNumber()} \{cline.getProduct().getDescription()} \{cline.getProduct().getPrice()}");
 		}
-		System.out.println("Total price ="+getT());
+		System.out.println("Total price = "+getT());
 	}
 
 	public void addProduct(Product product) {
-		for (CartLine cline : list) {
+		for (CartLine cline : cartLines) {
 			if (cline.getProduct().getProductNumber().equals(product.getProductNumber())) {
 				cline.setQuantity(cline.getQuantity()+1);
 				return;
@@ -27,11 +27,11 @@ public class ShoppingCart {
 		CartLine cline = new CartLine();
 		cline.setProduct(product);
 		cline.setQuantity(1);
-		list.add(cline);
+		cartLines.add(cline);
 	}
 
 	public void removeProduct(Product product) {
-		Iterator<CartLine> iter = list.iterator();
+		Iterator<CartLine> iter = cartLines.iterator();
 		while (iter.hasNext()){
 			CartLine cline = iter.next();
 			if (cline.getProduct().getProductNumber().equals(product.getProductNumber())){
@@ -47,10 +47,10 @@ public class ShoppingCart {
 
 	// get total price
 	public double getT(){
-		double tp = 0.0;
-		for (CartLine c : list) {
-			tp=tp+(c.getProduct().getPrice() * c.getQuantity());
+		double totalPrice = 0.0;
+		for (CartLine cartLine : cartLines) {
+			totalPrice=totalPrice+(cartLine.getProduct().getPrice() * cartLine.getQuantity());
 		}
-		return tp;
+		return totalPrice;
 	}
 }
